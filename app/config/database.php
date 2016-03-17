de6f3ead6f3340dca90b465a2c9c06cb3555d031
root <root@xposeserver.www.xpose.co.ke>
2016-03-17 16:45:51 +0300

    all

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'xpo',
+			'database'  => 'xpoz',
 			'username'  => 'root',
 			'password'  => 'mysql',
 			'port' => '3306',
