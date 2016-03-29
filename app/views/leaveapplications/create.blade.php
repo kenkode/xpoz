3f4107dc29523d041fa3440524afecf556b4d61f
arapmelly <jacqchumo@gmail.com>
2016-03-29 03:35:28 -0400

    leave emails

--- app/views/leaveapplications/create.blade.php
+++ app/views/leaveapplications/create.blade.php
@@ -48,21 +48,35 @@
         </div>
 
 
+
+        
+
+
+
         <div class="form-group">
                         <label for="username">Start Date <span style="color:red">*</span></label>
                         <div class="right-inner-addon ">
                         <i class="glyphicon glyphicon-calendar"></i>
-                        <input required class="form-control datepicker21" readonly="readonly" placeholder="" type="text" name="applied_start_date" id="applied_start_date" value="">
+                        <input required class="form-control datepicker21 appliedstartdate" readonly="readonly" placeholder="" type="text" name="applied_start_date" id="applied_start_date" value="">
                     </div>
        </div>
 
+
+       <div class="form-group">
+                        <label for="username">Days<span style="color:red">*</span></label>
+                        
+                     
+                        <input required class="form-control days"  placeholder="" type="text" name="days" id="days" value="">
+                    
+       </div>
+       
 
 
        <div class="form-group">
                         <label for="username">End Date <span style="color:red">*</span></label>
                         <div class="right-inner-addon ">
                         <i class="glyphicon glyphicon-calendar"></i>
-                        <input required class="form-control datepicker21" readonly="readonly" placeholder="" type="text" name="applied_end_date" id="applied_end_date" value="">
+                        <input required class="form-control enddate" readonly="readonly" placeholder="" type="text" name="applied_end_date" id="enddate"  id="end_date">
                     </div>
        </div>
 
@@ -83,6 +97,95 @@
   </div>
 
 </div>
+
+
+<script type="text/javascript">
+
+    $(document).ready(function() {
+
+        $('#days').keyup(function(){
+          var start_date = $("#applied_start_date").val();
+          var days = $("#days").val();
+          
+          alert(start_date);
+          
+
+          
+
+    });
+
+    });
+</script>
+
+
 @stop
+
+
+
+<script type="text/javascript">
+
+
+
+
+
+
+
+
+    
+
+
+
+/*
+$(document).ready(function(){
+
+   
+   alert('days');
+            
+    $("#days").keyup(function(){
+
+        //var start_date = $("#applied_start_date").val();
+
+        alert('days');
+
+        /*
+        var base_url = 'http://localhost/seller/public/clients/edit/'+id;
+
+        $.ajax({
+
+            type : 'GET',
+            url :  base_url,
+            dataType : 'json',
+            success : function(data){
+                  
+                $("#supplierid").val(data.id);
+                $("#supplierpaymentterms :selected").text(data.payment_terms);
+                $("#suppliername").val(data.name);
+                $("#supplieremail").val(data.email);
+                $("#supplierphone").val(data.phone);
+                $("#suppliercontactperson").val(data.contact_person);
+                $("#suppliercontactpersonphone").val(data.contact_person_phone);
+                
+            },
+
+            error : function(xhr, ajaxOptions, thrownError){
+
+                alert(xhr.status);
+            }
+
+        });
+
+        
+    event.preventDefault();
+
+    */
+
+    //});
+
+
+
+//});
+
+
+</script>
 
 
